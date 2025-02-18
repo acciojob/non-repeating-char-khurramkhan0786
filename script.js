46de5eac3797c2663db624f14e2ba298cdc2b230
khurram khan <153985864+khurramkhan0786@users.noreply.github.com>
2025-02-19 01:23:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-}
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
+for(let i=0;i<input.length;i++){
+	let isRepeated = false;
+	for(let j=0;j<input.length;j++){
+		if(i !== j && input[i] === input[j]){
+			isRepeated = true;
+			break;
+		}
+	}
+	if(!isRepeated){
+		return input[i];
+	}
+}
+	return null;
+}
